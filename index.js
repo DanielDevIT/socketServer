814512d5757de962059e1c6cd1d758b08e516259
Reallisticus <jmakin64@gmail.com>
2022-12-20 13:39:13 +0200

    test ssl

--- index.js
+++ index.js
@@ -1,68 +1,75 @@
-import { Server } from "socket.io";
+import { createServer } from 'https'
+import { readFileSync } from 'fs'
+import { Server } from 'socket.io'
 
-const io = new Server({
+const httpsServer = createServer({
+  key: readFileSync('/etc/letsencrypt/live/socket.foxd.org/privkey.pem'),
+  cert: readFileSync('/etc/letsencrypt/live/socket.foxd.org/fullchain.pem'),
+})
+
+const io = new Server(httpsServer, {
   cors: {
-    origin: "*",
+    origin: '*',
   },
-});
+})
 
-let onlineUsers = [{ username: "init", socketId: "init" }];
+let onlineUsers = [{ username: 'init', socketId: 'init' }]
 const addNewUser = (username, socketId) => {
   if (!onlineUsers.some((user) => user.username === username)) {
-    onlineUsers.push({ username, socketId });
+    onlineUsers.push({ username, socketId })
   }
-};
+}
 
 const removeUser = (socketId) => {
-  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
-};
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId)
+}
 
 const getUser = (username) => {
-  return onlineUsers.find((user) => user.username === username);
-};
+  return onlineUsers.find((user) => user.username === username)
+}
 
-io.on("connection", (socket) => {
-  socket.on("newUser", (username) => {
-    addNewUser(username, socket.id);
-  });
+io.on('connection', (socket) => {
+  socket.on('newUser', (username) => {
+    addNewUser(username, socket.id)
+  })
   // Should fire when users click on the action buttons on the singleTicket page
-  socket.on("sendNotification", ({ senderName, receiverName, type }) => {
-    const receiver = getUser(receiverName);
-    if (typeof receiver !== "undefined") {
-      io.to(receiver.socketId).emit("getNotification", {
+  socket.on('sendNotification', ({ senderName, receiverName, type }) => {
+    const receiver = getUser(receiverName)
+    if (typeof receiver !== 'undefined') {
+      io.to(receiver.socketId).emit('getNotification', {
         senderName,
         type,
-      });
+      })
     } else {
       setTimeout(function () {
         //Do the same function with a delay, otherwise the user won't recieve a notification :((
-        socket.on("sendNotification", ({ senderName, receiverName, type }) => {
-          const receiver = getUser(receiverName);
-          if (typeof receiver !== "undefined") {
-            io.to(receiver.socketId).emit("getNotification", {
+        socket.on('sendNotification', ({ senderName, receiverName, type }) => {
+          const receiver = getUser(receiverName)
+          if (typeof receiver !== 'undefined') {
+            io.to(receiver.socketId).emit('getNotification', {
               senderName,
               type,
-            });
+            })
           } else {
-            console.log("reciever is not defined");
+            console.log('reciever is not defined')
           }
-        });
-      }, 5000);
+        })
+      }, 5000)
     }
-  });
+  })
 
   //Useless for now
-  socket.on("sendText", ({ senderName, receiverName, text }) => {
-    const receiver = getUser(receiverName);
-    io.to(receiver.socketId).emit("getText", {
+  socket.on('sendText', ({ senderName, receiverName, text }) => {
+    const receiver = getUser(receiverName)
+    io.to(receiver.socketId).emit('getText', {
       senderName,
       text,
-    });
-  });
+    })
+  })
 
-  socket.on("disconnect", () => {
-    removeUser(socket.id);
-  });
-});
+  socket.on('disconnect', () => {
+    removeUser(socket.id)
+  })
+})
 
-io.listen(5000);
+httpsServer.listen(5000)
